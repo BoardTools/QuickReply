a261b00bb36bea59761a90d13a4cbf89b9953a3b
William Jacoby <bonelifer@gmail.com>
2014-07-25 19:44:30 -0500

    Translate to English

--- language/en/permissions_quickreply.php
+++ language/en/permissions_quickreply.php
@@ -1,10 +1,10 @@
 <?php
 /**
 *
-* quickreply [Russian]
+* quickreply [English]
 *
 * @package quickreply
-* @copyright (c) 2014 Татьяна5
+* @copyright (c) 2014 William Jacoby (bonelifer)
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
 *
 */
@@ -23,12 +23,12 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACP_QUICKREPLY'				=> 'Быстрый ответ',
-	'ACP_QUICKREPLY_EXPLAIN'		=> 'Настройки быстрого ответа',
+	'ACP_QUICKREPLY'				=> 'Quick Reply',
+	'ACP_QUICKREPLY_EXPLAIN'		=> 'Quick Reply Settings',
 
-	'ACL_A_QUICKREPLY'			=> 'Может изменять настройки быстрого ответа',
-	'ACL_F_QR_CHANGE_SUBJECT'	=> 'Может изменять заголовок сообщения',
-	'ACL_M_QR_CHANGE_SUBJECT'	=> 'Может изменять заголовок сообщения',
+	'ACL_A_QUICKREPLY'			=> 'Can change the settings of the Quick Reply',
+	'ACL_F_QR_CHANGE_SUBJECT'	=> 'Can modify the message header',
+	'ACL_M_QR_CHANGE_SUBJECT'	=> 'Can modify the message header',
 ));
 
 ?>
