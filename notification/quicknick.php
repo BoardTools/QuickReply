6eb27b743b444a5d7ebf53706407180ddac1634d
lavigor <lavigor@users.noreply.github.com>
2017-06-09 13:00:48 +0300

    Fix quicknick notifications.

--- notification/quicknick.php
+++ notification/quicknick.php
@@ -12,73 +12,90 @@ namespace boardtools\quickreply\notification;
 class quicknick extends \phpbb\notification\type\quote
 {
 	/**
-	* Get notification type name
-	*
-	* @return string
-	*/
+	 * Get notification type name
+	 *
+	 * @return string
+	 */
 	public function get_type()
 	{
 		return 'boardtools.quickreply.notification.type.quicknick';
 	}
 
 	/**
-	* regular expression to match to find usernames
-	*
-	* @var string
-	*/
-	protected static $regular_expression_match = '#\[ref(.*?)\]<\/s>(.+?)<e>\[\/ref#ui';
-
-	/**
-	* Language key used to output the text
-	*
-	* @var string
-	*/
+	 * Language key used to output the text
+	 *
+	 * @var string
+	 */
 	protected $language_key = 'NOTIFICATION_QUICKNICK';
 
 	/**
-	* Notification option data (for outputting to the user)
-	*
-	* @var bool|array False if the service should use it's default data
-	* 					Array of data (including keys 'id', 'lang', and 'group')
-	*/
+	 * Notification option data (for outputting to the user)
+	 *
+	 * @var bool|array False if the service should use it's default data
+	 *                    Array of data (including keys 'id', 'lang', and 'group')
+	 */
 	public static $notification_option = array(
-		'lang'	=> 'NOTIFICATION_TYPE_QUICKNICK',
-		'group'	=> 'NOTIFICATION_GROUP_POSTING',
+		'lang'  => 'NOTIFICATION_TYPE_QUICKNICK',
+		'group' => 'NOTIFICATION_GROUP_POSTING',
 	);
 
 	/**
-	* Is available
-	*/
+	 * Is available
+	 */
 	public function is_available()
 	{
 		return true;
 	}
 
 	/**
-	* Find the users who want to receive notifications
-	*
-	* @param array $post Data from submit_post
-	* @param array $options Options for finding users for notification
-	*
-	* @return array
-	*/
+	 * Get a list of mentioned users
+	 *
+	 * @param  string $xml Parsed text
+	 * @return string[] List of users
+	 */
+	private function get_mentioned_users($xml)
+	{
+		$usernames = array();
+		if (strpos($xml, '<REF ') === false)
+		{
+			return $usernames;
+		}
+
+		$dom = new \DOMDocument;
+		$dom->loadXML($xml);
+		$xpath = new \DOMXPath($dom);
+		foreach ($xpath->query('//REF') as $username)
+		{
+			$usernames[] = $username->textContent;
+		}
+
+		return $usernames;
+	}
+
+	/**
+	 * Find the users who want to receive notifications
+	 *
+	 * @param array $post    Data from submit_post
+	 * @param array $options Options for finding users for notification
+	 *
+	 * @return array
+	 */
 	public function find_users_for_notification($post, $options = array())
 	{
 		$options = array_merge(array(
-			'ignore_users'		=> array(),
+			'ignore_users' => array(),
 		), $options);
 
-		$usernames = false;
+		$usernames = $this->get_mentioned_users($post['post_text']);
 
-		preg_match_all(static::$regular_expression_match, $post['post_text'], $usernames);
-
-		if (empty($usernames[2]))
+		if (empty($usernames))
 		{
 			return array();
 		}
 
-		$usernames[2] = array_unique($usernames[2]);
-		$usernames = array_map('utf8_clean_string', $usernames[2]);
+		$usernames = array_unique($usernames);
+
+		$usernames = array_map('utf8_clean_string', $usernames);
 
 		$users = $this->get_ids_of_usernames($usernames, $post['poster_id']);
 
@@ -86,9 +103,13 @@ class quicknick extends \phpbb\notification\type\quote
 	}
 
 	/**
-	* @return array
-	*/
-	private function get_ids_of_usernames($usernames, $poster_id)
+	 * Get an array of users' IDs from an array of their usernames
+	 *
+	 * @param array $usernames Array of usernames
+	 * @param int   $poster_id User ID of current post author (that should not be notified)
+	 * @return int[]
+	 */
+	protected function get_ids_of_usernames($usernames, $poster_id)
 	{
 		$users = array();
 
@@ -107,10 +128,10 @@ class quicknick extends \phpbb\notification\type\quote
 	}
 
 	/**
-	* Get email template
-	*
-	* @return string|bool
-	*/
+	 * Get email template
+	 *
+	 * @return string|bool
+	 */
 	public function get_email_template()
 	{
 		return '@boardtools_quickreply/quicknick';
